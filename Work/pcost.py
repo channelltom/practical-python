2fd6d8c5d1986ee00befbeb9db582b8bca8e5b6d
Tom Channell <61183045+channelltom@users.noreply.github.com>
2020-10-20 21:08:21 +1100

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,35 @@
 # pcost.py
 #
 # Exercise 1.27
+# pcost.py
+#
+# Exercise 1.27
+
+import csv
+import sys
+def portfolio_cost(filename):
+    total_cost = 0.0
+
+    with open(filename, 'rt') as f:
+        rows=csv.reader(f)
+        headers = next(rows)
+        for line in f:
+            row=line.split(',')
+            for rowno, row in enumerate(rows, start=1):
+                record = dict(zip(headers, row))
+                try:
+                    n_shares = int(record['shares'])
+                    share_cost = float(record['price'])
+                    total_cost += n_shares * share_cost
+                except ValueError:
+                    print(f'Row {rowno}: Could not convert {row}')
+                    
+    return total_cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = input('Enter a filename: ')
+
+cost = portfolio_cost(filename)
+print('Total Cost:', cost)
