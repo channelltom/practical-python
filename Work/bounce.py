9aa1868ccf2adbb158c2f39027ae17e2e9983435
Tom Channell <61183045+channelltom@users.noreply.github.com>
2020-10-20 21:25:50 +1100

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+bounce = 1
+height_of_bounce = 60 # Meters
+
+while bounce < 11:
+    print (bounce, round(height_of_bounce))
+    bounce = bounce + 1
+    height_of_bounce = height_of_bounce * 0.6
