6e7292d3fe51e163e03b45bdd7d7bc82ff80e3f1
Tom Channell <61183045+channelltom@users.noreply.github.com>
2020-10-22 18:16:20 +1100

    fileparse module functionality.
    updated to use 'Fileparse' module in reading functions.

--- Work/report.py
+++ Work/report.py
@@ -1,48 +1,22 @@
-# report.py
-#
-# Exercise 2.4
-
 import csv
+from fileparse import parse_csv
+
 
 def read_portfolio(filename):
 	'''
 	Read a stock portfolio into a list of dictionaries with keys
 	name, shares and price.
 	'''
-	
-	portfolio = []
-	
-	with open(filename) as f:
-		rows = csv.reader(f)
-		headers = next(rows)
-		
-		for row in rows:
-			record = dict(zip(headers, row))
-			stock = {
-				'name': record['name'],
-				'shares': int(record['shares']),
-				'price': float(record['price'])
-			}
-			portfolio.append(stock)
-	
+	portfolio = parse_csv(filename, select=['name', 'shares', 'price'], types=[str, int, float])
 	return portfolio
-
 
 def read_prices(filename):
 	'''
 	Read a CSV file of price data into a dict mapping names to prices.
 	'''
-	
-	prices = {}
-	
-	with open(filename) as f:
-		rows = csv.reader(f)
-		for row in rows:
-			try:
-				prices[row[0]] = float(row[1])
-			except IndexError:
-				pass
+	prices = dict(parse_csv(filename, types=[str, float], has_headers=False))
 	return prices
+
 
 def make_report_data(portfolio, prices):
 	'''
